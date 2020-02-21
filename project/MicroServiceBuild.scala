4e28e4c4586dd82847862d7c004f86bd4720607b
yuan zhang <yuanzhang910@hotmail.com>
2020-02-21 11:55:14 +0000

    AIV-1758: upgrade library

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -1,3 +1,4 @@
+import play.sbt.PlayImport._
 import sbt._
 
 object MicroServiceBuild extends Build with MicroService {
@@ -9,24 +10,21 @@ object MicroServiceBuild extends Build with MicroService {
 
 private object AppDependencies {
 
-  import play.sbt.PlayImport._
-
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.13.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.20.0-play-25"
+    "uk.gov.hmrc" %% "bootstrap-play-25"    % "5.1.0",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.23.0-play-25"
   )
 
   val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1",
-    "com.github.tomakehurst" % "wiremock" % "1.58" % "test,it",
-    "org.mockito" % "mockito-core" % "2.27.0" % "test,it",
-    "org.pegdown" % "pegdown" % "1.6.0" % "test,it",
-    "org.scalatest" %% "scalatest" % "3.0.7" % "test,it",
-    "uk.gov.hmrc" %% "service-integration-test" % "0.9.0-play-25" % "test,it",
-    "uk.gov.hmrc" %% "reactivemongo-test" % "4.14.0-play-25" % "test,it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"           % "test,it",
+    "org.scalatest"           %% "scalatest"                % "3.0.7"           % "test,it",
+    "com.github.tomakehurst"  %  "wiremock"                 % "1.58"            % "test,it",
+    "org.mockito"             %  "mockito-core"             % "2.27.0"          % "test,it",
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"           % "test,it",
+    "uk.gov.hmrc"             %% "service-integration-test" % "0.9.0-play-25"   % "test,it",
+    "uk.gov.hmrc"             %% "reactivemongo-test"       % "4.16.0-play-25"  % "test,it"
   )
 
   def apply() = compile ++ test
 }
-
