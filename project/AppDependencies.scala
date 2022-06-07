0bfff2b19c34a04bd0cedb299109f4453f2bb109
ShaneTN <58428357+ShaneTN@users.noreply.github.com>
2022-06-07 16:39:41 +0100

    GG-6399 updated logger + lib versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28"    % "5.14.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28"    % "5.20.0",
     "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "government-gateway-test"  % "4.6.0-play-28"   % "test,it",
+    "uk.gov.hmrc" %% "government-gateway-test"  % "4.9.0"   % "test,it",
     "uk.gov.hmrc" %% "reactivemongo-test"       % "5.0.0-play-28"   % "test,it",
     "org.mockito" %% "mockito-scala-scalatest"  % "1.16.37"         % "test,it",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2" % "test,it"
