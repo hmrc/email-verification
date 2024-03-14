b6449a1da88136f7585ea686c2db28daa6b7ebf5
ianmcnicholas <ian.mcnicholas@digital.hmrc.gov.uk>
2024-03-14 15:17:05 +0000

    GG-7595 : Upgrading to Play 3.0
    addressing comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ object AppDependencies {
   private val bootstrapVersion = "8.4.0"
   private val hmrcMongoVersion = "1.7.0"
 
-  val compile: Seq[ModuleID] = Seq(
+  private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion
   )
 
-  val test: Seq[ModuleID] = Seq(
+  private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "government-gateway-test-play-30" % "6.0.0",
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30"          % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"         % hmrcMongoVersion,
