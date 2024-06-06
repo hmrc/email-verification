93b1ed6aabadad62cb9205d55f2c2082e74cd0a6
Jin Bal <gurjinder.bal@digital.hmrc.gov.uk>
2024-06-06 15:55:34 +0100

    GG-7817 satisfy scoverage

--- test/uk/gov/hmrc/emailverification/tasks/DropVerifiedEmailCollectionTaskSpec.scala
+++ test/uk/gov/hmrc/emailverification/tasks/DropVerifiedEmailCollectionTaskSpec.scala
@@ -20,13 +20,16 @@ import config.AppConfig
 import org.apache.pekko.actor.ActorSystem
 import org.mockito.scalatest.MockitoSugar
 import org.scalatest.concurrent.Eventually
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.wordspec.AnyWordSpec
+import play.api.Logger
 import uk.gov.hmrc.emailverification.repositories.VerifiedEmailMongoRepository
+import uk.gov.hmrc.gg.test.LogCapturing
 
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.Future
-class DropVerifiedEmailCollectionTaskSpec extends AnyWordSpec with MockitoSugar with Eventually {
+class DropVerifiedEmailCollectionTaskSpec extends AnyWordSpec with MockitoSugar with Eventually with LogCapturing with Matchers {
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(5, Seconds), interval = Span(500, Millis))
   trait Setup {
     implicit val as: ActorSystem = ActorSystem()
@@ -42,6 +45,21 @@ class DropVerifiedEmailCollectionTaskSpec extends AnyWordSpec with MockitoSugar 
       eventually {
         verify(mockRepo).drop()
         succeed
+      }
+    }
+
+    "log error when fails" in new Setup {
+      withCaptureOfLoggingFrom(Logger(Class.forName("uk.gov.hmrc.emailverification.tasks.DropVerifiedEmailCollectionTask"))) { logs =>
+        when(appConfigMock.dropVerifiedEmailCollectionEnabled).thenReturn(true)
+        when(appConfigMock.dropVerifiedEmailCollectionDelaySecs).thenReturn(1)
+        when(mockRepo.drop()).thenReturn(Future.failed(new Exception("failed")))
+        new DropVerifiedEmailCollectionTask(appConfigMock, mockRepo)
+        eventually {
+          verify(mockRepo).drop()
+          val logMessages = logs.map(_.getMessage)
+          logMessages should contain("[GG-7817] Failed to drop the collection 'verifiedEmail'. Reason: failed")
+          succeed
+        }
       }
     }
   }
