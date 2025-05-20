0eaaade54e2db96b6367ce05bf8aab2bf256c34b
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-05-20 16:20:19 +0100

    CIR-2099 Fix bobby rule warning

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
   private val bootstrapVersion = "9.0.0"
-  private val hmrcMongoVersion = "2.0.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
